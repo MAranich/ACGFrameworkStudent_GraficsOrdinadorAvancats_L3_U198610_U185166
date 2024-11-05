6b9184f7a1752ef6b5a183e156ba1af0ac55ec9c
MAranich <marcel.aranich01@estudiant.upf.edu>
2024-11-05 11:25:09 +0100

    Moved noise functions to the bottom

--- res/shaders/volume_rand_hetero.fs
+++ res/shaders/volume_rand_hetero.fs
@@ -32,67 +32,15 @@ vec2 intersectAABB(vec3 rayOrigin, vec3 rayDir, vec3 boxMin, vec3 boxMax);
 vec3 world_to_local(vec3 world, mat4 model); 
 
 // NOISE FUNCTIONS ///////////////////////////////////////////////////////
-float hash1( float n )
-{
-    return fract( n*17.0*fract( n*0.3183099 ) );
-}
+float hash1( float n ); 
 
-float noise( vec3 x )
-{
-    vec3 p = floor(x);
-    vec3 w = fract(x);
-    
-    vec3 u = w*w*w*(w*(w*6.0-15.0)+10.0);
-    
-    float n = p.x + 317.0*p.y + 157.0*p.z;
-    
-    float a = hash1(n+0.0);
-    float b = hash1(n+1.0);
-    float c = hash1(n+317.0);
-    float d = hash1(n+318.0);
-    float e = hash1(n+157.0);
-    float f = hash1(n+158.0);
-    float g = hash1(n+474.0);
-    float h = hash1(n+475.0);
-
-    float k0 =   a;
-    float k1 =   b - a;
-    float k2 =   c - a;
-    float k3 =   e - a;
-    float k4 =   a - b - c + d;
-    float k5 =   a - c - e + g;
-    float k6 =   a - b - e + f;
-    float k7 = - a + b + c - d + e - f - g + h;
-
-    return -1.0+2.0*(k0 + k1*u.x + k2*u.y + k3*u.z + k4*u.x*u.y + k5*u.y*u.z + k6*u.z*u.x + k7*u.x*u.y*u.z);
-}
+float noise( vec3 x ); 
 
 #define MAX_OCTAVES 16
 
-float fractal_noise( vec3 P, float detail )
-{
-    float fscale = 1.0;
-    float amp = 1.0;
-    float sum = 0.0;
-    float octaves = clamp(detail, 0.0, 16.0);
-    int n = int(octaves);
+float fractal_noise( vec3 P, float detail ); 
 
-    for (int i = 0; i <= MAX_OCTAVES; i++) {
-        if (i > n) continue;
-        float t = noise(fscale * P);
-        sum += t * amp;
-        amp *= 0.5;
-        fscale *= 2.0;
-    }
-
-    return sum;
-}
-
-float cnoise( vec3 P, float scale, float detail )
-{
-    P *= scale;
-    return clamp(fractal_noise(P, detail), 0.0, 1.0);
-}
+float cnoise( vec3 P, float scale, float detail ); 
 
 //////////////////////////////////////////////////////////////////////////////////////////
 
@@ -138,7 +86,7 @@ void main() {
    // vec4 ret = vec4(u_color.xyz, u_color.w * (1.0 - transmitansse)); 
 
 
-    optical_thickness = (optical_thickness  * u_absortion_coef); 
+    optical_thickness = (optical_thickness * u_absortion_coef); 
     float transmittance = exp(-optical_thickness);
 
     vec4 ret = vec4(u_bg_color.xyz * (transmittance), 1.0); 
@@ -172,6 +120,62 @@ vec3 world_to_local(vec3 world, mat4 model) {
 }
 
 
+float hash1( float n )
+{
+    return fract( n*17.0*fract( n*0.3183099 ) );
+}
 
+float noise( vec3 x )
+{
+    vec3 p = floor(x);
+    vec3 w = fract(x);
+    
+    vec3 u = w*w*w*(w*(w*6.0-15.0)+10.0);
+    
+    float n = p.x + 317.0*p.y + 157.0*p.z;
+    
+    float a = hash1(n+0.0);
+    float b = hash1(n+1.0);
+    float c = hash1(n+317.0);
+    float d = hash1(n+318.0);
+    float e = hash1(n+157.0);
+    float f = hash1(n+158.0);
+    float g = hash1(n+474.0);
+    float h = hash1(n+475.0);
 
+    float k0 =   a;
+    float k1 =   b - a;
+    float k2 =   c - a;
+    float k3 =   e - a;
+    float k4 =   a - b - c + d;
+    float k5 =   a - c - e + g;
+    float k6 =   a - b - e + f;
+    float k7 = - a + b + c - d + e - f - g + h;
 
+    return -1.0+2.0*(k0 + k1*u.x + k2*u.y + k3*u.z + k4*u.x*u.y + k5*u.y*u.z + k6*u.z*u.x + k7*u.x*u.y*u.z);
+}
+
+float fractal_noise( vec3 P, float detail )
+{
+    float fscale = 1.0;
+    float amp = 1.0;
+    float sum = 0.0;
+    float octaves = clamp(detail, 0.0, 16.0);
+    int n = int(octaves);
+
+    for (int i = 0; i <= MAX_OCTAVES; i++) {
+        if (i > n) continue;
+        float t = noise(fscale * P);
+        sum += t * amp;
+        amp *= 0.5;
+        fscale *= 2.0;
+    }
+
+    return sum;
+}
+
+float cnoise( vec3 P, float scale, float detail )
+{
+    P *= scale;
+    return clamp(fractal_noise(P, detail), 0.0, 1.0);
+}
